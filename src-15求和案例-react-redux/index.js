228f11f5e4a2995a16ada559f0477a1e3126ee66
lichaojie <chaojie.lee@outlook.com>
2021-11-05 14:06:18 +0800

    去掉 subscribe

--- src-15求和案例-react-redux/index.js
+++ src-15求和案例-react-redux/index.js
@@ -4,8 +4,3 @@ import App from "./App";
 import store from "./redux/store";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// 检测 redux 状态，若 redux 状态改变，则重新渲染 App 组件
-store.subscribe(() => {
-  ReactDOM.render(<App />, document.getElementById("root"));
-})
