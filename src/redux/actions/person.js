fb8721fe989911e4f29518029efb782512c45a25
lichaojie <chaojie.lee@outlook.com>
2021-11-08 18:04:11 +0800

    hooks

--- src/redux/actions/person.js
+++
@@ -1,3 +0,0 @@
-import { ADD_PERSON } from "../constant";
-
-export const addPerson = personsObj => ({type: ADD_PERSON, payload: personsObj});
--- src/redux/constant.js
+++
@@ -1,7 +0,0 @@
-/**
- * 此模块用于定义 redux 中的常量，防止写错
- */
-
-export const INCREMENT = 'increment'
-export const DECREMENT = 'decrement'
-export const ADD_PERSON = 'add_person'
