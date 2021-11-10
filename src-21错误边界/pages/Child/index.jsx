cbba35dfd9c6d4226f06634378d776fd2fee5632
lichaojie <chaojie.lee@outlook.com>
2021-11-10 11:41:32 +0800

    错误边界，捕获子组件的错误

---
+++ src-21错误边界/pages/Child/index.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Child extends Component {
+  state = {
+    // 正确的写法
+    // users: [
+    //   {id: 1, name: 'tony', age: 18},
+    //   {id: 2, name: 'tom', age: 20},
+    //   {id: 3, name: 'jerry', age: 22},
+    // ]
+
+    // 假如后端传递的是纯字符串形式，此时我们渲染就会报错
+    'users': '[]'
+  }
+
+  // 父组件 getDerivedStateFromError 不能捕获生命周期外的错误，比如：
+  // test 什么都不是
+  // test()
+
+  render() {
+    return (
+      <div className="child">
+        <h3>Child组件</h3>
+        {
+          this.state.users.map((item) => {
+            return <h4 key={item.id}>{item.name} --- {item.age}</h4>
+          }) 
+        }
+      </div>
+    );
+  }
+}
